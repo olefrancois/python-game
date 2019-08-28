945ec6f8ed79c6dce47e437bd1f3e9af20e2c996
olivier lefrancois <olivier.lefrancois@smartpanda.com>
2019-08-28 14:25:42 +0200

    chapitre 1 : exercice_1_1

--- chapitre_1/exo_1_1.py
+++ chapitre_1/exo_1_1.py
@@ -1,1 +1,20 @@
-print('Here we go !!!')
+
+from random import *
+
+print("Excercice 1.1 : 1.")
+print(randint(1, 6) + randint(1, 6) + randint(1, 6), end=" ")
+print()
+print()
+
+print("Excercice 1.1 : 2.")
+for x in range(50):
+    print(randint(1, 6) + randint(1, 6) + randint(1, 6), end=" ")
+print()
+print()
+
+print("Excercice 1.1 : 3.")
+resultat = 0
+for x in range(200):
+    print(randint(1, 8) + randint(1, 8), end=" ")
+print()
+print()
